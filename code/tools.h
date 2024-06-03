a434d8f2886f141221178a8a158fbd68505da5bd
panliang206 <2069066570@qq.com>
2024-06-03 19:19:14 +0800

    32

--- code/tools.h
+++ code/tools.h
@@ -5,7 +5,7 @@
 void control(){
     system("cls");
     printf("请选择你要进行的操作:\n");
-    printf("1.商品信息查询\n2.添加货物功能\n3.删除货物功能\n4.选择商品促销\n5.退出\n\n");
+    printf("1.商品信息查询\n2.添加货物功能\n3.删除货物功能\n4.选择商品促销\n5.查询过期商品\n6.退出\n\n");
     int choice;
     scanf("%d",&choice);
     switch(choice){
@@ -21,6 +21,8 @@ void control(){
         case 4:
             abc();
         case 5:
+            park();
+        case 6:
             printf("谢谢使用，再见！\n");
 
             // 退出系统前保存数据到文件
