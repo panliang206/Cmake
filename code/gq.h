2d6dd57ff61d942a618c3a52c49e4b9524d57dd0
panliang206 <2069066570@qq.com>
2024-06-03 19:05:51 +0800

    65

---
+++ code/gq.h
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<time.h>
+#include"create_data.h"
+struct Product {
+    char name[60];
+    time_t expirationDate;
+};
+
+void park()
+{ for (int j = 1; j < 7 ; j++){ 
+     switch(j){
+        case 1: 
+            printf("1.休闲零食\n");
+        case 2:
+            printf("2.饮料冲调\n");
+        case 3:
+            printf("5.牛奶面包\n");
+        case 4:
+            printf("4.营养早餐\n");
+        case 5:
+            printf("5.牛奶面包\n");
+        case 6:
+            printf("6.速冻食品\n");
+    for (int i = 1; i < 11; i++) 
+    {
+    if(a[i][j]="")
+    {break;}
+    
+    struct Product sp;
+    strcpy(sp.name,"a[i][j].name");
+    struct tm expirationDate = {0};
+    expirationDate.tm_year = a[i][j].jz.year-1900;
+    expirationDate.tm_mon = a[i][j].jz.month-1;
+    expirationDate.tm_mday = a[i][j].jz.day;
+    sp.expirationDate = mktime(&expirationDate); 
+
+    time_t currentTime;
+    time(&currentTime);
+
+    if (difftime(sp.expirationDate, currentTime) > 0) {
+        printf("%s 没有过期\n", sp.name);}
+    else {
+        printf("%s 已经过期\n", sp.name);}
+    }
+    }
+    return 0;
+ }
+   
